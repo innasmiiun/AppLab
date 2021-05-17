e8fad81190be9d5c61e334f92eccf7cba711f0b0
innasmiiun <innasmiyun@gmail.com>
2021-05-17 10:11:35 +0300

    Updated files.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-let item = document.getElementsByClassName("question_item")
+let item = document.getElementsByClassName("question_item");
 
 for (let i = 0; i < item.length; i++) {
-    item[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let block = this.nextElementSibling;
-        if (block.style.display === "block") {
-            block.style.display = "none";
-        } else {
-            block.style.display = "block";
-        }
-    });
-}
-function toggleMenu() {
-    let menu = document.getElementById("toggleMenu");
-    if (menu.style.display === "grid") {
-        menu.style.display = "none";
+  item[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let block = this.nextElementSibling;
+    if (block.style.display === "block") {
+      block.style.display = "none";
     } else {
-        menu.style.display = "grid";
+      block.style.display = "block";
     }
+  });
+}
+
+function toggleMenu() {
+  let menu = document.getElementById("toggleMenu");
+  if (menu.style.display === "grid") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "grid";
+  }
 }
